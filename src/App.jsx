a930d631e94b09c198cc81f466dc38c419334669
natedunn2230 <ndunn2230@gmail.com>
2019-07-24 19:00:30 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+export default class App extends React.Component {
+
+  render(){
+    return (
+      <h1>A Map should be here later</h1>
+    );
+  }
+  
+}
+
