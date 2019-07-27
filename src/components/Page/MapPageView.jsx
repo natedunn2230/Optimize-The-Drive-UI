774b583a0081824078a40697bb4ec46fcc62378e
natedunn2230 <ndunn2230@gmail.com>
2019-07-26 21:21:08 -0400

    Added more styling

--- src/components/Page/MapPageView.jsx
+++ src/components/Page/MapPageView.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import MapView from '../App/Map/MapView'
 
+import './MapPage.css';
+
 export default class MapPageView extends React.Component {
 
     render(){
         return(
-            <MapView/>
+            <div id="map-page">
+                <span id="page-header">Optimize The Drive</span>
+                <MapView/>
+            </div>
+            
         );
     }
 }
